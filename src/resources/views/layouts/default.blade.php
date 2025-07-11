249e37595c408cec2af16a5641494ca5d31acef0
Eyüp <epsilonelittr@gmail.com>
2025-07-12 01:23:40 +0300

    style: update navigation container width to use container class

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -15,7 +15,7 @@
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
         <!-- Navigation Bar -->
         <nav class="bg-white dark:bg-gray-800 shadow-sm dark:shadow-gray-700">
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="container mx-auto px-4">
                 <div class="flex justify-between h-16">
                     <div class="flex">
                         <div class="flex-shrink-0 flex items-center">
